59cc6d552782bd9e29844e82844dda238474533c
amitmusicband <68270163+amitmusicband@users.noreply.github.com>
2020-07-14 17:34:02 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -27,7 +27,7 @@ $('.nav li a').bind('click', function(e){
 $(document).ready(function(){
 	
 	var obj = document.createElement("audio");
-	obj.src = "~/audio/dilkadariya.mpeg";
+	obj.src = "https://www.youtube.com/watch?v=9-AKLAfpjrI";
 	obj.volume = 1;
 	obj.autoPlay = true;
 	obj.preLoad = true;       
